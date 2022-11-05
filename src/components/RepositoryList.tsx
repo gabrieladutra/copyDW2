2c47fa7303ece5e1b9a3d3320fca045c9f662159
gabrieladutra <gabriela_ddutra@outlook.com>
2022-11-05 16:08:58 -0300

    Added tsc logic

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,16 @@ import { RepositoryItem } from "./RepositoryItem"
 import '../styles/repositories.scss'
 import { useState, useEffect } from "react"
 
+
+interface Repository{
+  id: number;
+  name: string;
+        description:string;
+        html_url: string;
+}
+
 export function RepositoryList(){
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
    useEffect(() => {
     fetch('https://api.github.com/users/gabrieladutra/repos').then(response => response.json()).then(data=>setRepositories(data))
    }, [])
--- webpack.config.js
+++ webpack.config.js
@@ -5,43 +5,42 @@ const isDevelopment = process.env.NODE_ENV !== 'production'
 const ReactRefreshWebPackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 module.exports = {
-	mode: isDevelopment ? 'development' : 'production',
-	devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-	entry: path.resolve(__dirname, 'src', 'index.jsx'),
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
-	 },
-	resolve: {
-		extensions: ['.js', '.jsx'],
-	},
-	devServer: {
-		static: path.resolve(__dirname, 'public'),
+    mode: isDevelopment ? 'development' : 'production',
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    devServer: {
+        static: path.resolve(__dirname, 'public'),
         hot: true
-	},
-	plugins: [
+    },
+    plugins: [
         isDevelopment && new ReactRefreshWebPackPlugin,
-		new HtmlWebPlugin ({
-		    template: path.resolve(__dirname, 'public', 'index.html')
-		})
-	].filter(Boolean),
-	module: {
-		rules: [
-			{
-				test: /\.jsx$/,
-				exclude: /node_modules/,
-				use: {
+        new HtmlWebPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ].filter(Boolean),
+    module: {
+        rules: [{
+                test: /\.(j|t)sx$/,
+                exclude: /node_modules/,
+                use: {
                     loader: 'babel-loader',
                     options: {
                         plugins: [isDevelopment && require('react-refresh/babel')].filter(Boolean)
                     }
                 }
-			},
+            },
             {
-				test: /\.scss$/,
-				exclude: /node_modules/,
-				use: ['style-loader', 'css-loader', 'sass-loader']
-			}
-		]
-	}
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    }
 }
